14e3e637429adfa1344d7af10cdae0dfc1ca2986
BoberKrzysztof <k.bober@onet.pl>
2022-05-12 11:22:56 +0200

    Logowanie

---
+++ app/src/main/java/com/example/besafe/LogowanieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.besafe
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LogowanieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logowanie)
+    }
+}
