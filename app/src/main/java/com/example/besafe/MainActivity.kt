ec1f362de9242db89fe4182ad48a81c566e1b76d
Kuba <244887@student.pwr.edu.pl>
2022-05-15 16:00:18 +0200

    Poprawione ekrany, dodane przejścia

--- app/src/main/java/com/example/besafe/MainActivity.kt
+++ app/src/main/java/com/example/besafe/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.besafe
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        goLogin()
+        goRegister()
+    }
+
+
+    private fun goLogin() {
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LogowanieActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun goRegister() {
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RejestracjaActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
