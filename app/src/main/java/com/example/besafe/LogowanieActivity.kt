99e77ec1f4bbaeac2cc2ff80b54e8802ae4e295c
BoberKrzysztof <k.bober@onet.pl>
2022-05-19 02:05:30 +0200

    Logowanie

--- app/src/main/java/com/example/besafe/LogowanieActivity.kt
+++ app/src/main/java/com/example/besafe/LogowanieActivity.kt
@@ -4,50 +4,50 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import at.favre.lib.crypto.bcrypt.BCrypt
 import com.google.firebase.database.*
 
 
 class LogowanieActivity : AppCompatActivity() {
 
-    val databaseReference: DatabaseReference = FirebaseDatabase.getInstance()
-        .getReferenceFromUrl("https://besafe-ecb67-default-rtdb.europe-west1.firebasedatabase.app/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logowanie)
 
-        val emailInput = findViewById<EditText>(R.id.PhoneNumberLoginInput)
+        val phoneInput = findViewById<EditText>(R.id.PhoneNumberLoginInput)
         val passwordInput = findViewById<EditText>(R.id.PasswordLoginInput)
         val loginButton = findViewById<Button>(R.id.LoginButton)
 
         loginButton.setOnClickListener {
-            val email = emailInput.text.toString()
+            val phone = phoneInput.text.toString()
             val password = passwordInput.text.toString()
-        }
-    }
 
+            val databaseReference: DatabaseReference = FirebaseDatabase.getInstance()
+                .getReferenceFromUrl("https://besafe-ecb67-default-rtdb.europe-west1.firebasedatabase.app/users/$phone")
 
-    fun logowanie() {
+            databaseReference.addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
 
-        val phoneInput = findViewById<EditText>(R.id.PhoneNumberLoginInput)
-        val phonetxt = phoneInput.text.toString().trim()
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        val hash = snapshot.child("password").value.toString()
+                        val result = BCrypt.verifyer().verify(password.toCharArray(), hash)
+                        if (result.verified) {
+                            Toast.makeText(baseContext,"Witaj ${snapshot.child("name").value.toString()}",Toast.LENGTH_SHORT).show()
+                            phoneInput.text.clear()
+                            passwordInput.text.clear()
+                        } else {
+                            Toast.makeText(baseContext,"Nie udało się zalogować",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
 
-        var login: HashMap<*, *>
-        var potwierdzenie: Boolean
-        databaseReference.child("users").get().addOnSuccessListener {
-            Log.i("firebase", "Got value ${it.value}")
-            login = it.value as HashMap<*, *>
-
-//            for ((key, value) in login) {
-//                println("$key = $value")
-//            }
-
-            potwierdzenie = login.containsKey(phonetxt)
-            if (potwierdzenie) {
-                databaseReference.child("users").child(phonetxt)
-                    .child("phone number").get()
-            }
         }
     }
 }
