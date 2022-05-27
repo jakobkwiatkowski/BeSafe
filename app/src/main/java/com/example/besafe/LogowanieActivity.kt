02d37152e5455b4e23ba1df0fe51aecad90afd97
mateusz-olek <Mateusz!1998>
2022-05-27 22:14:42 +0200

    zabezpieczenia - logowanie i rejestracja, kosmetyka

--- app/src/main/java/com/example/besafe/LogowanieActivity.kt
+++ app/src/main/java/com/example/besafe/LogowanieActivity.kt
@@ -1,17 +1,17 @@
 package com.example.besafe
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import at.favre.lib.crypto.bcrypt.BCrypt
 import com.google.firebase.database.*
 
 
 class LogowanieActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,29 +25,78 @@ class LogowanieActivity : AppCompatActivity() {
             val phone = phoneInput.text.toString()
             val password = passwordInput.text.toString()
 
-            val databaseReference: DatabaseReference = FirebaseDatabase.getInstance()
-                .getReferenceFromUrl("https://besafe-ecb67-default-rtdb.europe-west1.firebasedatabase.app/users/$phone")
+            when {
 
-            databaseReference.addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
+                (phone.isEmpty() || password.isEmpty()) -> {
+                    Toast.makeText(this, "Proszę wypełnić wszystkie pola", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        val hash = snapshot.child("password").value.toString()
-                        val result = BCrypt.verifyer().verify(password.toCharArray(), hash)
-                        if (result.verified) {
-                            Toast.makeText(baseContext,"Witaj ${snapshot.child("name").value.toString()}",Toast.LENGTH_SHORT).show()
-                            phoneInput.text.clear()
-                            passwordInput.text.clear()
-                        } else {
-                            Toast.makeText(baseContext,"Nie udało się zalogować",Toast.LENGTH_SHORT).show()
+                (password.length < 6) -> {
+                    Toast.makeText(
+                        this,
+                        "Hasło zawiera przynajmniej sześć znaków",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    passwordInput.text.clear()
+                }
+
+                (!phone.replace("\\s".toRegex(), "").isDigitsOnly()) -> {
+                    Toast.makeText(
+                        this,
+                        "Numer telefonu składa się wyłącznie z cyfr",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    phoneInput.text.clear()
+                }
+
+                else -> {
+
+                    val databaseReference: DatabaseReference = FirebaseDatabase.getInstance()
+                        .getReferenceFromUrl("https://besafe-ecb67-default-rtdb.europe-west1.firebasedatabase.app/users/$phone")
+
+                    databaseReference.addValueEventListener(object : ValueEventListener {
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
                         }
-                    }
-                }
-            })
 
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            if (snapshot.exists()) {
+                                val hash = snapshot.child("password").value.toString()
+                                val result = BCrypt.verifyer().verify(password.toCharArray(), hash)
+                                if (result.verified) {
+                                    Toast.makeText(
+                                        baseContext,
+                                        "Witaj ${snapshot.child("name").value.toString()}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    phoneInput.text.clear()
+                                    passwordInput.text.clear()
+                                    goStart()
+                                } else {
+                                    Toast.makeText(
+                                        baseContext,
+                                        "Nie udało się zalogować - nieprawidłowe hasło",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            } else {
+                                Toast.makeText(
+                                    baseContext,
+                                    "Nie udało się zalogować - nieprawidłowy numer telefonu",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    })
+
+                }
+            }
         }
+    }
+
+    private fun goStart() {
+        val intent = Intent(this, StartActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/besafe/MainActivity.kt
+++ app/src/main/java/com/example/besafe/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.besafe
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
