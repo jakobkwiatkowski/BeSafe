676f8089c5e13f494d62db04626404c082eeb0c9
mateusz-olek <Mateusz!1998>
2022-06-02 00:45:00 +0200

    Zabezpieczenie - nr telefonu

--- app/src/main/java/com/example/besafe/RejestracjaActivity.kt
+++ app/src/main/java/com/example/besafe/RejestracjaActivity.kt
@@ -1,7 +1,6 @@
 package com.example.besafe
 
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -9,8 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.isDigitsOnly
 import at.favre.lib.crypto.bcrypt.BCrypt
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
 class RejestracjaActivity : AppCompatActivity() {
 
@@ -22,18 +20,13 @@ class RejestracjaActivity : AppCompatActivity() {
         setContentView(R.layout.activity_rejestracja)
 
         val registerButton = findViewById<Button>(R.id.RegisterButton)
-        val phoneInput = findViewById<EditText>(R.id.PhoneInput)
-        val passwordInput = findViewById<EditText>(R.id.PasswordInput)
-        val nameInput = findViewById<EditText>(R.id.NameInput)
-        val surnameInput = findViewById<EditText>(R.id.SurnameInput)
-        val codeInput = findViewById<EditText>(R.id.CodeInput)
-
         registerButton.setOnClickListener {
             register()
         }
     }
 
     fun register() {
+
         val phoneInput = findViewById<EditText>(R.id.PhoneInput)
         val passwordInput = findViewById<EditText>(R.id.PasswordInput)
         val nameInput = findViewById<EditText>(R.id.NameInput)
@@ -46,80 +39,99 @@ class RejestracjaActivity : AppCompatActivity() {
         val passwordtxt = passwordInput.text.toString().trim()
         val codetxt = codeInput.text.toString().trim()
 
+        databaseReference.child("users").child(phonetxt).get().addOnSuccessListener {
+            Log.i("firebase", "Got value ${it.value}")
 
-        when {
+            when {
 
-            (nametxt.isEmpty() || surnametxt.isEmpty() || phonetxt.isEmpty() || passwordtxt.isEmpty() || codetxt.isEmpty()) -> {
-                Toast.makeText(this, "Proszę wypełnić wszystkie pola", Toast.LENGTH_SHORT).show()
-            }
-
-            (passwordtxt.length < 6) -> {
-                Toast.makeText(
-                    this,
-                    "Hasło powinno zawierać przynajmniej sześć znaków",
-                    Toast.LENGTH_SHORT
-                ).show()
-                passwordInput.text.clear()
-            }
-
-            (!phonetxt.replace("\\s".toRegex(), "").isDigitsOnly()) -> {
-                Toast.makeText(
-                    this,
-                    "Numer telefonu powinien składać się wyłącznie z cyfr",
-                    Toast.LENGTH_SHORT
-                ).show()
-                phoneInput.text.clear()
-            }
-
-            else -> {
-
-                // Hashowanie
-                val passHash = BCrypt.withDefaults().hashToString(12, passwordtxt.toCharArray())
-
-                var klucze: HashMap<*, *>
-                var potwierdzenie: Boolean
-                databaseReference.child("ActivationKeys").get().addOnSuccessListener {
-                    Log.i("firebase", "Got value ${it.value}")
-                    klucze = it.value as HashMap<*, *>
-                    potwierdzenie = klucze.containsKey(codetxt)
-                    if (potwierdzenie) {
-                        Toast.makeText(
-                            this,
-                            "Poprawny indywidualny kod weryfikacyjny",
-                            Toast.LENGTH_SHORT
-                        ).show()
-
-                        databaseReference.child("users").child(phonetxt).child("verification code")
-                            .setValue(codetxt)
-                        databaseReference.child("users").child(phonetxt).child("name")
-                            .setValue(nametxt)
-                        databaseReference.child("users").child(phonetxt).child("surname")
-                            .setValue(surnametxt)
-                        databaseReference.child("users").child(phonetxt).child("phone number")
-                            .setValue(phonetxt)
-                        // Hashowanie
-                        databaseReference.child("users").child(phonetxt).child("password")
-                            .setValue(passHash)
-                        // Usunięcie wprowadzonego klucza weryfikacyjnego spośród dostępnych
-                        databaseReference.child("ActivationKeys").child(codetxt).removeValue()
-
-                        Toast.makeText(this, "Rejestracja przebiegła pomyślnie", Toast.LENGTH_SHORT)
-                            .show()
-                        finish()
-                    } else {
-                        Toast.makeText(
-                            this,
-                            "Kod weryfikacyjny nie występuje w bazie - pozyskaj nowy",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        codeInput.text.clear()
-                    }
-                }.addOnFailureListener {
-                    Log.e("firebase", "Error getting data", it)
+                (nametxt.isEmpty() || surnametxt.isEmpty() || phonetxt.isEmpty() || passwordtxt.isEmpty() || codetxt.isEmpty()) -> {
+                    Toast.makeText(this, "Proszę wypełnić wszystkie pola", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
+                (passwordtxt.length < 6) -> {
+                    Toast.makeText(
+                        this,
+                        "Hasło powinno zawierać przynajmniej sześć znaków",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    passwordInput.text.clear()
+                }
+
+                (!phonetxt.replace("\\s".toRegex(), "").isDigitsOnly()) -> {
+                    Toast.makeText(
+                        this,
+                        "Numer telefonu powinien składać się wyłącznie z cyfr",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    phoneInput.text.clear()
+                }
+
+                (it.value?.equals(null) == false) -> {
+                    Toast.makeText(
+                        this,
+                        "Numer telefonu występuje w bazie",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    phoneInput.text.clear()
+                }
+
+                else -> {
+
+                    // Hashowanie
+                    val passHash = BCrypt.withDefaults().hashToString(12, passwordtxt.toCharArray())
+
+                    var klucze: HashMap<*, *>
+                    var potwierdzenie: Boolean
+                    databaseReference.child("ActivationKeys").get().addOnSuccessListener {
+                        Log.i("firebase", "Got value ${it.value}")
+                        klucze = it.value as HashMap<*, *>
+                        potwierdzenie = klucze.containsKey(codetxt)
+                        if (potwierdzenie) {
+                            Toast.makeText(
+                                this,
+                                "Poprawny indywidualny kod weryfikacyjny",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            databaseReference.child("users").child(phonetxt)
+                                .child("verification code")
+                                .setValue(codetxt)
+                            databaseReference.child("users").child(phonetxt).child("name")
+                                .setValue(nametxt)
+                            databaseReference.child("users").child(phonetxt).child("surname")
+                                .setValue(surnametxt)
+                            databaseReference.child("users").child(phonetxt).child("phone number")
+                                .setValue(phonetxt)
+                            // Hashowanie
+                            databaseReference.child("users").child(phonetxt).child("password")
+                                .setValue(passHash)
+                            // Usunięcie wprowadzonego klucza weryfikacyjnego spośród dostępnych
+                            databaseReference.child("ActivationKeys").child(codetxt).removeValue()
+
+                            Toast.makeText(
+                                this,
+                                "Rejestracja przebiegła pomyślnie",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Kod weryfikacyjny nie występuje w bazie - pozyskaj nowy",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            codeInput.text.clear()
+                        }
+                    }.addOnFailureListener {
+                        Log.e("firebase", "Error getting data", it)
+                    }
+
+                }
             }
+        }.addOnFailureListener {
+            Log.e("firebase", "Error getting data", it)
         }
     }
-
 }
